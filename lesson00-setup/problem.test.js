15833c4b22a664b02743a9ea8e57fc0f54fc29aa
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-01-22 11:03:32 -0500

    completed the warmup.js and problem.js

--- lesson00-setup/problem.test.js
+++ lesson00-setup/problem.test.js
@@ -1,5 +1,5 @@
 const sayHello = require("./problem");
-
+// This function should return hello world
 describe("sayHello", () => {
   test("should return 'Hello, World!'", () => {
     expect(sayHello()).toBe("Hello, World!");
