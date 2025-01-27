aff059673a94b3b4b52d18b6005d92d550a56b9f
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-01-27 12:17:57 -0500

    completed the warmup.js and problem.js

--- lesson01-tdd/problem.js
+++ lesson01-tdd/problem.js
@@ -10,8 +10,44 @@
  * @param {string} str - The input string.
  * @returns {boolean} - True if the string is a palindrome, false otherwise.
  */
+
 function isPalindrome(str) {
-  // your code here
+  // If the data type of the input is not a string
+  if (typeof str !== "string") {
+    //  turn it into a string
+    str = str.toString();
+  }
+
+  // Reasign the string and lowercase it and replace everything that is not letters and nums with an empty string
+  str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  // console.log(str)
+
+  // split the string, reverse it and rejoin it
+  let splitStr = str.split("").reverse().join("");
+  // console.log(splitStr)
+
+  // Compare the reversed string with the input string
+  if (splitStr === str) {
+    return true;
+  }
+
+  // Return false if all those conditions are not true
+  return false;
 }
+
+// console.log(isPalindrome(" ")); // true
+// console.log(isPalindrome("A")); // true
+// console.log(isPalindrome("mom")); // true
+// console.log(isPalindrome("sat")); // false
+
+// console.log(isPalindrome("mo m")); // true
+// console.log(isPalindrome("Mo m")); // true
+// console.log(isPalindrome("Mom")); // true
+
+// console.log(isPalindrome("!mOm!")); // true
+// console.log(isPalindrome(123321)); // true
+console.log(isPalindrome("!&!")); // true
+// console.log(isPalindrome("Brother")); // false
+// console.log(isPalindrome("Yay it works!")); // false
 
 module.exports = isPalindrome;
--- lesson01-tdd/warmup.js
+++ lesson01-tdd/warmup.js
@@ -4,18 +4,116 @@
 // 12 => '************'
 // 3 => "***"
 
+function numOfAsterisks(num) {
+  // return "*".repeat(num)
+
+  // Declare a string variable
+  let asterisks = "";
+  // Iterate up until the num
+  for (let i = 0; i < num; i++) {
+    // in each iterateion add the asterisk string to the variable
+    asterisks += "*";
+  }
+  // return the variable
+  return asterisks;
+}
+
+// console.log(numOfAsterisks(5));
+// console.log(numOfAsterisks(12));
+// console.log(numOfAsterisks(3));
+
 // 2. Given an integer, n, between 3 and 100,
 // return a line n characters long where the first and last character are '*' and all the middle characters are a space.
 
 // 5 => '*   *'
 // 12 => '*          *'
 
-// 3. Given two integers, n and m, create n lines of m characters. The first and last line should be made of all '*' and all the middle lines should start and end with '*' and have all spaces in the middle
+function firstAndLastAreAstrisks(num) {
+  let results = "";
+  for (let i = 0; i < num; i++) {
+    if (i === 0 || i === num - 1) {
+      results += "*";
+    } else {
+      results += " ";
+    }
+  }
+  return results;
+}
 
-// 5,5 => *****
-//        *   *
-//        *   *
-//        *   *
-//        *****
+// console.log(firstAndLastAreAstrisks(5)); // *   *
+// console.log(firstAndLastAreAstrisks(12)); // *          *
+
+/* 
+                       5     5         5          5
+3. Given two integers, n and m, create n lines of m characters. The first and last line should be made of all '*' and all the middle lines should start and end with '*' and have all spaces in the middle
+
+
+5,5 => *****     3,3 => ***     1,2 => **     4,3 => ***
+       *   *            * *                          * *
+       *   *            ***                          * *
+       *   *                                         ***
+       *****
+*/
+
+/**
+ * @param {Number} n - The number of lines
+ * @param {Number} m - The number of characters
+ * The first and last line should be made of all "*"
+ * All other lines should start and end with "*" and have space in between
+ */
+const nlinesOfMCharacters = (n, m) => {
+  // bring in both previous functions
+  let lines = numOfAsterisks(m); //.repeat(n); // .split("");
+  // console.log(lines); // ****
+  let characters = firstAndLastAreAstrisks(m);
+  // console.log(characters); // * *
+  let results = "";
+  // console.log(results);
+
+  // console.log(
+  //   (results =
+  //     lines +
+  //     "\n" +
+  //     characters +
+  //     "\n" +
+  //     characters +
+  //     "\n" +
+  //     characters +
+  //     "\n" +
+  //     lines)
+  // );
+  // console.log((results = lines + "\n" + characters + "\n" + lines));
+  // console.log(
+  //   (results = lines + "\n" + characters + "\n" + characters + "\n" + lines)
+  // );
+  for (let i = 0; i < n; i++) {
+    if (i > 0) {
+      results += "\n";
+    }
+    if (i === 0 || i === n - 1) {
+      results += lines;
+    } else {
+      results += characters;
+    }
+  }
+  /*
+  i = 0 = results= lines     
+  i = 1 = results= lines + "\n" + characters 
+  i = 2 = results= lines + "\n" + characters + "\n" + characters 
+  
+
+  
+  
+  */
+
+  console.log(results);
+};
+
+// console.log(nlinesOfMCharacters(5, 5));
+// console.log(nlinesOfMCharacters(3,3))
+// console.log(nlinesOfMCharacters(1,2))
+console.log(nlinesOfMCharacters(4, 3));
 
 // Console.log the result of each of the above functions to test them. Run your file with node warmup.js and check the terminal for the correct output.
+
+module.exports = { numOfAsterisks, firstAndLastAreAstrisks };
